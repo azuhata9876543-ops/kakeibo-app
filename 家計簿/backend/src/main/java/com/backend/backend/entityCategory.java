0a707711de0971ff074687423dcf5d5031798e40
azuhata9876543-ops <azuhata9876543@gmail.com>
2026-07-27 17:11:02 +0900

    entityを作成中

---
+++ 家計簿/backend/src/main/java/com/backend/backend/entityCategory.java
@@ -0,0 +1,11 @@
+package com.backend.backend;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+@Entity
+public class entityCategory {
+    
+}
---
+++ 家計簿/backend/src/main/java/com/backend/backend/repository.java
@@ -0,0 +1,6 @@
+import org.springframework.date.jpa/repository.JpaRepsitory;
+
+@Repository
+public interface repository extends JpaRepsitory<Transaction, Long>{
+    
+}
