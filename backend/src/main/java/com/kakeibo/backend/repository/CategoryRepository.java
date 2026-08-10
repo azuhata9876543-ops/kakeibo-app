87b6862ef7a93bf7d5af0fdc4eee7e381706586e
azuhata9876543-ops <azuhata9876543@gmail.com>
2026-08-10 13:49:12 +0900

    .envをgitの管理対象から除外

--- backend/src/main/java/com/kakeibo/backend/repository/CategoryRepository.java
+++ backend/src/main/java/com/kakeibo/backend/repository/CategoryRepository.java
@@ -2,9 +2,7 @@ package com.kakeibo.backend.repository;
 
 import com.kakeibo.backend.entity.EntityCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<EntityCategory, Long> {
     
 }
--- backend/src/main/java/com/kakeibo/backend/repository/UserRepository.java
+++ backend/src/main/java/com/kakeibo/backend/repository/UserRepository.java
@@ -2,9 +2,7 @@ package com.kakeibo.backend.repository;
 
 import com.kakeibo.backend.entity.EntityUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<EntityUser, String> {
     
 }
