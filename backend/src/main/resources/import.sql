a292bc6ad69e9352871d5719876884b6de704285
azuhata9876543-ops <azuhata9876543@gmail.com>
2026-08-10 11:22:02 +0900

    古い履歴と新しい履歴を合流

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO user (username, password) VALUES ('test', '$2a$10$7Q0Z1J5F6G8H9I0J1K2L3M4N5O6P7Q8R9S0T1U2V3W4X5Y6Z7A8B9C')
+
+INSERT INTO category(category_type, category) VALUES('INCOME', '給与')
+INSERT INTO category(category_type, category) VALUES('INCOME', '副収入')
+
+INSERT INTO category(category_type, category) VALUES('EXPENSE', '固定費')
+INSERT INTO category(category_type, category) VALUES('EXPENSE', '食費')
+INSERT INTO category(category_type, category) VALUES('EXPENSE', '日用品')
+INSERT INTO category(category_type, category) VALUES('EXPENSE', '医療')
+INSERT INTO category(category_type, category) VALUES('EXPENSE', '装飾')
+INSERT INTO category(category_type, category) VALUES('EXPENSE', '車')
+INSERT INTO category(category_type, category) VALUES('EXPENSE', '特別費')
+INSERT INTO category(category_type, category) VALUES('EXPENSE', 'その他')
